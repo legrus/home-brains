09b2f2bea627a262759e8088017a2f9f2c3d9440
Oleg Petukhov <legrus@gmail.com>
2013-03-12 20:26:09 +0400

    Fixed broken port open to output feature

--- src/home_brains/gpio_sink.py
+++ src/home_brains/gpio_sink.py
@@ -14,6 +14,10 @@ GPIO.setmode(GPIO.BCM)
 class GpioSink(Pipe):
     """Set Raspberry Pi GPIOs"""
 
+    def __init__(self, _id, _param, _inputs = [], _options = {}):
+        super(GpioSink, self).__init__(_id, _param, _inputs, _options)
+        GPIO.setup(self.gpio(), GPIO.OUT)
+
     def gpio(self):
         return self.param
 
