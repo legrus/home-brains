098626d4a83aa5ef01e97e33b40855eec6c3e69a
Oleg Petukhov <legrus@gmail.com>
2013-03-19 00:18:37 +0400

    Voice recognition with google voice API

--- src/home_brains/voice_pipe.py
+++ src/home_brains/voice_pipe.py
@@ -1,8 +1,11 @@
 # -*- coding: UTF-8 -*-
 # https://github.com/legrus/home-brains, legrus, 2013
 
+import json
+import logging
+import urllib2
+
 from home_brains import Variable
-from urllib import urlencode
 
 
 class VoicePipe(Variable):
@@ -14,5 +17,40 @@ class VoicePipe(Variable):
         super(VoicePipe, self).preprocess()  # check inputs are sane
 
         # TODO
+        # working cmdline:
+        # wget --post-file out.flac
+        #   --header="Content-Type: audio/x-flac; rate=16000"
+        #   -O speech.txt
+        #   http://www.google.com/speech-api/v1/recognize?lang=ru
+
+        self.error = False
+        self.value = None
+
+        if len(self.inputs) > 0:
+            filename = self.inputs[0].value
+
+            try:
+                lang = 'ru'
+                url = 'http://www.google.com/speech-api/v1/recognize?lang=%s' % lang
+                header = {'Content-Type': 'audio/x-flac; rate=16000"'}
+                flac = open(filename, 'rb').read()
+                req = urllib2.Request(url, flac, header)
+                response = urllib2.urlopen(req).read().decode('utf-8')
+                speech = json.loads(response)
+
+                logging.debug(" * Google speech response: %s", speech)
+
+                if 'status' in speech and speech['status'] == 0:
+                    self.value = speech['hypotheses'][0]['utterance'].encode('utf-8')
+
+            except urllib2.HTTPError, e:
+                logging.debug("Error: %s", e)
+                self.error = True
+            except ValueError, e:
+                logging.debug("Error: %s", e)
+                self.error = True
+
+        if self.value is None:
+            self.error = True
 
         super(VoicePipe, self).process()
