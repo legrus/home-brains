fe25c8dbbc587efcc70e1b01f1c9b18370093a33
Oleg Petukhov <legrus@gmail.com>
2013-03-18 13:37:16 +0400

    Cosmetics

--- src/traffic.py
+++ src/traffic.py
@@ -2,6 +2,10 @@
 # -*- coding: UTF-8 -*-
 # https://github.com/legrus/home-brains, legrus, 2013
 
+import logging
+import sys
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
 from home_brains import *
 
 # Simple tests
