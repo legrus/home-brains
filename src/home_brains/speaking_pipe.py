4e7abc21c97b9907b29a9b52c7489faf34b829ba
Oleg Petukhov <legrus@gmail.com>
2013-03-19 00:19:54 +0400

    Added automatic language detection. Still not perfect for short phrases, better fix language in options {}

--- src/home_brains/speaking_pipe.py
+++ src/home_brains/speaking_pipe.py
@@ -2,7 +2,9 @@
 # https://github.com/legrus/home-brains, legrus, 2013
 
 from home_brains import WebSource
+import logging
 from urllib import urlencode
+from guess_language import guessLanguageTag
 
 
 class SpeakingPipe(WebSource):
@@ -11,11 +13,28 @@ class SpeakingPipe(WebSource):
     '''
     #TODO: cache media files
 
+    def detect_language(self):
+        lang_from_options = self.get_option('lang')
+
+        text_for_guess_lang = ((self.inputs[0].value or " ") + " ") * 5  # this library handles long texts better somehow
+        guessed_lang = guessLanguageTag(text_for_guess_lang)
+        if guessed_lang == 'UNKNOWN':
+            guessed_lang = None
+
+        logging.debug(" * detect_language: %s %s %s ", lang_from_options, guessed_lang, 'en')
+        return lang_from_options or guessed_lang or 'en'
+
     def create_media_url(self):
+        if self.inputs[0].value is None:
+            return None
+
+        lang = self.detect_language()
+
+        logging.debug(" * ok, val=%s, lang=%s", self.inputs[0].value, lang)
 
         query = urlencode({
             'q': self.inputs[0].value,
-            'tl': 'en'  # TODO detect language
+            'tl': lang
         })
 
         return 'http://translate.google.com/translate_tts?%s' % query
